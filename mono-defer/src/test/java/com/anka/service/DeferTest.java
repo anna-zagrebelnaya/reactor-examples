96b2e97d6d95ce68fc12080b1d871a469929f928
anna-zagrebelnaya <annzagrebelnaya@gmail.com>
2018-06-11 23:07:24 +0300

    refactor test for "Mono.defer"

--- mono-defer/src/test/java/com/anka/service/DeferTest.java
+++ mono-defer/src/test/java/com/anka/service/DeferTest.java
@@ -1,38 +1,58 @@
 package com.anka.service;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import reactor.core.publisher.Flux;
 
+import java.time.Clock;
+import java.time.Instant;
 
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DeferTest {
+
+    private static final Instant instant1 = Instant.ofEpochMilli(1L);
+    private static final Instant instant2 = Instant.ofEpochMilli(2L);
+
+    @Mock
+    private Clock clock;
+
+    @Before
+    public void init() {
+        when(clock.instant()).thenReturn(instant1).thenReturn(instant2);
+    }
+
+    @Test
+    public void testJustFlux() {
+        Flux<Instant> flux = getCurrentTime();
+
+        flux.subscribe(i -> Assert.assertEquals(instant1, i));
+        flux.subscribe(i -> Assert.assertEquals(instant1, i));
+    }
 
     @Test
     public void testDefer() {
-        Flux<Long> fluxWithDefer = Flux.defer(this::getCurrentTime);
-        Flux<Long> flux = getCurrentTime();
-        Flux<Long> fluxWithDeferAndCache = Flux.defer(this::getCurrentTime).cache();
+        Flux<Instant> flux = Flux.defer(this::getCurrentTime);
 
-        fluxWithDefer.subscribe(l -> System.out.println("1 fluxWithDefer:" + l));
-        flux.subscribe(l -> System.out.println("1 flux:" + l));
-        fluxWithDeferAndCache.subscribe(l -> System.out.println("1 fluxWithDeferAndCache:" + l));
-
-        fluxWithDefer.subscribe(l -> System.out.println("2 fluxWithDefer:" + l));
-        flux.subscribe(l -> System.out.println("2 flux:" + l));
-        fluxWithDeferAndCache.subscribe(l -> System.out.println("2 fluxWithDeferAndCache:" + l));
-
-        /* output:
-        1 fluxWithDefer:        1526239255393
-        1 flux:                 1526239255377
-        1 fluxWithDeferAndCache:1526239255413
-
-        2 fluxWithDefer:        1526239255413
-        2 flux:                 1526239255377
-        2 fluxWithDeferAndCache:1526239255413
-         */
+        flux.subscribe(i -> Assert.assertEquals(instant1, i));
+        flux.subscribe(i -> Assert.assertEquals(instant2, i));
     }
 
-    private Flux<Long> getCurrentTime() {
-        return Flux.just(System.currentTimeMillis());
+    @Test
+    public void testDeferAndCache() {
+        Flux<Instant> flux = Flux.defer(this::getCurrentTime).cache();
+
+        flux.subscribe(i -> Assert.assertEquals(instant1, i));
+        flux.subscribe(i -> Assert.assertEquals(instant1, i));
+    }
+
+    private Flux<Instant> getCurrentTime() {
+        return Flux.just(clock.instant());
     }
 
 }
